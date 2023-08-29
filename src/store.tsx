cd2c5820f31686e8ddd60c7203a94169d625ac7e
AlexTrubkina <sashatru99@gmail.com>
2023-08-29 12:50:14 +0300

    feat: add redux store

---
+++ src/store.tsx
@@ -0,0 +1,26 @@
+import {createStore, combineReducers, applyMiddleware} from "redux";
+import thunk from "redux-thunk";
+import {composeWithDevTools} from "redux-devtools-extension";
+
+import {
+    allGamesReducer,
+    categoryGamesReducer
+} from "./reducers/gameReducers";
+
+const reducer = combineReducers({
+    allGames: allGamesReducer,
+    categoryGames: categoryGamesReducer, 
+    
+});
+
+const initialState = {}
+
+const middleware = [thunk];
+
+const store = createStore(
+    reducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
