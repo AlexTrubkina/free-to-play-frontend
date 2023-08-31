b1e173c0795fb43cde9891ab598fec2df59d761f
AlexTrubkina <sashatru99@gmail.com>
2023-08-31 11:02:14 +0300

    refactor: add component for first screen in HomeScreen

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,24 +1,15 @@
 import React, { useEffect, useState } from "react";
 import LayoutPage from "../../layouts/LayOutPage";
 import {
-    Icon,
     Flex,
-    Button,
-    Container,
-    Grid,
-    GridItem,
-    Heading,
-    Text,
-    Box,
     Spinner
 } from "@chakra-ui/react";
 import { getAllGames } from "../../actions/gameActions";
-
-import { FaGamepad } from "react-icons/fa";
 import { useAppDispatch, useAppSelector } from "../../hooks/reduxHooks";
 import GameCards from "./Components/GameCards";
-import { Link } from "react-router-dom";
+
 import ErrorCard from "../../components/ErrorCard/ErrorCard";
+import FirstScreen from "./Components/FirstScreen";
 
 const HomeScreen = (): JSX.Element => {
     const dispatch = useAppDispatch();
@@ -32,67 +23,7 @@ const HomeScreen = (): JSX.Element => {
 
     return (
         <LayoutPage>
-            <Flex backgroundColor="gray.700" h="89vh" alignItems="center">
-                <Container maxW={{base: "80%", md: "90%"}}>
-                    <Grid
-                        alignItems="center"
-                        templateColumns={{ base: "1fr", lg: "1.5fr 1fr" }}
-                        gap="30px"
-                    >
-                        <GridItem>
-                            <Heading
-                                mb="20px"
-                                color="gray.100"
-                                size={{base: "2xl", md: "3xl"}}
-                                fontWeight="bold"
-                            >
-                                Сервис с бесплатными играми
-                            </Heading>
-                            <Text mb="20px" color="gray.100">
-                                Попробуйте его прямо сейчаc
-                            </Text>
-                            <Flex direction="row" flexWrap="wrap" gap="20px">
-                                <Link to="/games">
-                                    <Button colorScheme="gray" variant="solid">
-                                        Посмотреть все игры
-                                    </Button>
-                                </Link>
-                                <Link to="/games">
-                                    <Button colorScheme="linkedin">
-                                        Больше жанров
-                                    </Button>
-                                </Link>
-                            </Flex>
-                        </GridItem>
-                        <GridItem
-                            display={{ base: "none", lg: "grid" }}
-                            justifyContent="center"
-                        >
-                            <Flex
-                                direction="column"
-                                alignItems="center"
-                                gap="20px"
-                            >
-                                <Box
-                                    border="1px"
-                                    textAlign="center"
-                                    p="10px"
-                                    borderRadius="15px"
-                                    borderColor="blue.500"
-                                    color="blue.500"
-                                >
-                                    Откройте для себя множество игр
-                                </Box>
-                                <Icon
-                                    as={FaGamepad}
-                                    color="blue.500"
-                                    boxSize={150}
-                                />
-                            </Flex>
-                        </GridItem>
-                    </Grid>
-                </Container>
-            </Flex>
+            <FirstScreen />
             <Flex
                 justifyContent="center"
                 backgroundColor="gray.700"
